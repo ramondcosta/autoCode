ffcb79f778274e7d6166ac85e9cbbe063c9f6b04
ramondcosta <ramon.eumsm.costa@gmail.com>
2016-12-05 02:33:59 -0300

    buildCode function Improved

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+typedef struct parseNode{
+    string word;
+    parseNode* firstBranch;
+    parseNode* secondBranch;
+}*parseTree,parseNode;
+
+
 //char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
 string json;
 StaticJsonBuffer<1000> jsonBuffer;
@@ -12,13 +19,29 @@ JsonObject& jsonCallback(){
     JsonObject& root = jsonBuffer.parseObject(json);
     return root;
 }
+
+
 void buildCode(JsonObject& root){
     //JsonObject& root = *jsonPtr;
     const char* name = root["name"];
+    const char* exprStr = root["handler"]["str"];
+    const char* exprInt = root["handler"]["int"];
+    const char* exprBool = root["handler"]["bool"];
     printf("class " "%s{\n"
            "public:\n"
            "\t%s();\n"
-           "}\n",name,name);
+           "\tvoid handler(char* x){\n"
+           "\t\t*x = %s\n"
+           "\t}\n"
+           "\tvoid handler(int* x){\n"
+           "\t\t*x = %s\n"
+           "\t}\n"
+           "\tvoid handler(bool* x){\n"
+           "\t\t*x = %s\n"
+           "\t}\n"
+           "}\n"
+           ,name,name
+           ,exprStr,exprInt,exprBool);
 }
 
 #define print(STR) cout << STR
