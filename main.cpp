5803ed07e63bee11173cdab66477a8fed9b613c8
ramondcosta <ramon.eumsm.costa@gmail.com>
2016-12-05 17:48:51 -0300

    Now iterates over a sensor array

--- main.cpp
+++ main.cpp
@@ -4,22 +4,26 @@
 
 using namespace std;
 
-typedef struct parseNode{
+typedef struct eventNode{
     string word;
-    parseNode* firstBranch;
-    parseNode* secondBranch;
-}*parseTree,parseNode;
 
+    eventNode* next;
+}*eventList,eventNode;
+
+void event1(){
+
+}
 
 //char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
-string json;
-StaticJsonBuffer<1000> jsonBuffer;
 
-JsonObject& jsonCallback(){
+
+JsonObject& jsonCallback(string json){
+    StaticJsonBuffer<200000> jsonBuffer;
     JsonObject& root = jsonBuffer.parseObject(json);
     return root;
 }
 
+#define handlerTemplate(STR)"\tvoid handler(" STR " *x){\n" "\t\t*x = %s\n" "\t}\n"
 
 void buildCode(JsonObject& root){
     //JsonObject& root = *jsonPtr;
@@ -27,18 +31,13 @@ void buildCode(JsonObject& root){
     const char* exprStr = root["handler"]["str"];
     const char* exprInt = root["handler"]["int"];
     const char* exprBool = root["handler"]["bool"];
+
     printf("class " "%s{\n"
            "public:\n"
            "\t%s();\n"
-           "\tvoid handler(char* x){\n"
-           "\t\t*x = %s\n"
-           "\t}\n"
-           "\tvoid handler(int* x){\n"
-           "\t\t*x = %s\n"
-           "\t}\n"
-           "\tvoid handler(bool* x){\n"
-           "\t\t*x = %s\n"
-           "\t}\n"
+           handlerTemplate("char")
+           handlerTemplate("int")
+           handlerTemplate("bool")
            "}\n"
            ,name,name
            ,exprStr,exprInt,exprBool);
@@ -51,14 +50,33 @@ void buildCode(JsonObject& root){
 
 JsonObject* teste;
 
+/*void buildBlock(){
+    string json;
+    get(json);
+    JsonObject& root = jsonCallback(json);
+    buildCode(root);
+}*/
+
+void buildBlock(){
+    int i;
+    string json;
+    get(json);
+    JsonObject& root = jsonCallback(json);
+    if (!root.success()){
+      println("parseObject() failed - A famigerada merda");
+      return;
+    }
+    JsonArray& sensors = root["sensors"];
+
+
+    for(i = 0; i < sensors.size();i++){
+        buildCode(sensors[i]);
+    }
+}
+
 int main(){
 
-    get(json);
-
-    JsonObject& root = jsonCallback();
-
-    //teste
-    buildCode(root);
+    buildBlock();
 
     return 0;
 }
